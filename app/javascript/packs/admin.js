33f372c3f3b49d515bfda3bf0f619be8df3a2435
Masashi Fujita <masashifujita536@gmail.com>
2022-04-11 19:10:11 +0900

    adminlte3の初期設定

---
+++ app/javascript/packs/admin.js
@@ -0,0 +1,11 @@
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+
+import "@fortawesome/fontawesome-free/js/all.min"
+import "bootstrap"
+import "admin-lte"
+
+import "../stylesheets/admin.scss"
+
+Rails.start()
+ActiveStorage.start()
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
 
+// 全ての pack で jQuery を使えるようにする
+const webpack = require('webpack');
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
+
 module.exports = environment
