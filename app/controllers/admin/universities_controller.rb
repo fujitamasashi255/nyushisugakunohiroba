e0d8d68218da653fa950adf24cdb3190d6111fa8
Masashi Fujita <masashifujita536@gmail.com>
2022-04-13 21:25:23 +0900

    管理画面での大学作成機能を追加

---
+++ app/controllers/admin/universities_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class Admin::UniversitiesController < Admin::ApplicationController
+  before_action :set_university, only: %i[show]
+
+  def new
+    @university = University.new
+  end
+
+  def create
+    @university = University.new(university_params)
+    if @university.save
+      redirect_to [:admin, @university], success: "hoge"
+    else
+      render "admin/universities/new"
+    end
+  end
+
+  def show; end
+
+  private
+
+  def university_params
+    params.require(:university).permit(:name)
+  end
+
+  def set_university
+    @university = University.find(params[:id])
+  end
+end
