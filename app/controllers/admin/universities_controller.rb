fedb887e1a2719d0c4b4c3c8ad2cf0b451f3c00b
Masashi Fujita <masashifujita536@gmail.com>
2022-04-14 18:53:08 +0900

    管理画面での大学オブジェクトの詳細・編集・削除機能を追加

--- app/controllers/admin/universities_controller.rb
+++ app/controllers/admin/universities_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Admin::UniversitiesController < Admin::ApplicationController
-  before_action :set_university, only: %i[show]
+  before_action :set_university, only: %i[show edit destroy update]
 
   def new
     @university = University.new
@@ -10,7 +10,7 @@ class Admin::UniversitiesController < Admin::ApplicationController
   def create
     @university = University.new(university_params)
     if @university.save
-      redirect_to [:admin, @university], success: "hoge"
+      redirect_to [:admin, @university], success: t("messages.success_create_univ")
     else
       flash.now[:danger] = t("messages.fail_create_univ")
       render "admin/universities/new"
@@ -18,6 +18,22 @@ class Admin::UniversitiesController < Admin::ApplicationController
   end
 
   def show; end
+
+  def edit; end
+
+  def update
+    if @university.update(university_params)
+      redirect_to [:admin, @university], success: t("messages.success_update_univ")
+    else
+      flash.now[:danger] = t("messages.fail_update_univ")
+      render "admin/universities/edit"
+    end
+  end
+
+  def destroy
+    @university.destroy!
+    redirect_to new_admin_university_path, success: t("messages.success_destroy_univ")
+  end
 
   private
 
