f4c01931fe6664fc6bb681b652875d8062db56ae
Masashi Fujita <masashifujita536@gmail.com>
2022-04-21 18:12:58 +0900

    universityファクトリに、子レコードdepartmentを持つようなtraitを追加

--- spec/factories/universities.rb
+++ spec/factories/universities.rb
@@ -16,5 +16,27 @@
 FactoryBot.define do
   factory :university do
     sequence(:name) { |n| "test#{n}" }
+
+    # create(:university, :has_one_department, name: "hoge", department_name: "fuga")
+    trait :has_one_department do
+      transient do
+        department_name { "department_name" }
+      end
+
+      after(:create) do |university, evaluator|
+        university.departments << create(:department, name: evaluator.department_name)
+      end
+    end
+
+    # create(:university, :has_departments, name: "hoge", department_counts: 5)
+    trait :has_departments do
+      transient do
+        department_counts { 5 }
+      end
+
+      after(:create) do |university, evaluator|
+        university.departments << create_list(:department, evaluator.department_counts)
+      end
+    end
   end
 end
