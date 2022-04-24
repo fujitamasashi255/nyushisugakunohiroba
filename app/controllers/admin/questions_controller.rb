f1fee7137ba9c5866f59f9053f3ae78121332ba5
Masashi Fujita <masashifujita536@gmail.com>
2022-04-24 11:09:10 +0900

    問題新規作成画面での、大問番号、出題年、大学・区分、単元、登録機能を実装

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,7 +1,40 @@
 # frozen_string_literal: true
 
 class Admin::QuestionsController < Admin::ApplicationController
+  before_action :set_question, only: %i[show edit destroy update]
+
   def new
     @question = Question.new
+    @question.departments = []
+  end
+
+  def create
+    @question = Question.new(number: question_params[:number], set_year: question_params[:set_year])
+    @question.departments << Department.find(question_params[:department_ids].reject(&:blank?))
+    @question.add_units_to_association(question_params[:unit_ids].reject(&:blank?))
+    if @question.save
+      redirect_to [:admin, @question], success: t("flashes.question.success.create")
+    else
+      flash.now[:danger] = t("flashes.question.success.create")
+      render "admin/questions/new"
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:number, :set_year, department_ids: [], unit_ids: [])
+  end
+
+  def set_question
+    @question = Question.find(params[:id])
   end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -13,10 +13,21 @@
 class Question < ApplicationRecord
   validates :number, presence: true
   validates :set_year, presence: true
+  validate :departments_belong_to_same_university?
+  validate :unit?
+  validate :department?
 
   has_many :questions_departments_mediators, dependent: :destroy
   has_many :departments, through: :questions_departments_mediators
   has_many :questions_units_mediators, dependent: :destroy
+
+  def university
+    departments.present? ? departments.first.university : nil
+  end
+
+  def university?
+    university.present?
+  end
 
   def units
     Unit.find(questions_units_mediators.map(&:unit_id))
@@ -24,5 +35,28 @@ class Question < ApplicationRecord
 
   def unit_ids
     units.pluck(:id)
+  end
+
+  def add_units_to_association(unit_idz)
+    questions_units_mediators << unit_idz.map do |unit_id|
+      QuestionsUnitsMediator.new(unit_id:)
+    end
+  end
+
+  private
+
+  # questionのdepartmentが少なくとも1つはあること
+  def department?
+    errors.add(:base, "区分を登録してください") if department_ids.blank?
+  end
+
+  # questionのunitが少なくとも1つはあること
+  def unit?
+    errors.add(:base, "単元を登録してください") if unit_ids.blank?
+  end
+
+  # questionのdepartmentsが属するuniversityが1つだけかチェック
+  def departments_belong_to_same_university?
+    errors.add(:base, "異なる大学に登録することはできません") if departments.map(&:university_id).uniq.count > 1
   end
 end
