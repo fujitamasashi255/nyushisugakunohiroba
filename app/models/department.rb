3ce5f3b9e15549b36fe83a73bbdae7942cadc1f2
Masashi Fujita <masashifujita536@gmail.com>
2022-04-29 08:35:35 +0900

    department削除前に、それのみに関連するquestionを削除するコールバックを作成

--- app/models/department.rb
+++ app/models/department.rb
@@ -19,9 +19,20 @@
 #  fk_rails_...  (university_id => universities.id)
 #
 class Department < ApplicationRecord
+  # departmentを削除する前に、そのdepartmentのみに関連するquestionを削除する
+  before_destroy :destroy_association_question
+
   validates :name, presence: true
 
   belongs_to :university
   has_many :questions_departments_mediators, dependent: :destroy
   has_many :questions, through: :questions_departments_mediators
+
+  private
+
+  def destroy_association_question
+    questions.each do |question|
+      question.destroy if question.departments == [self]
+    end
+  end
 end
