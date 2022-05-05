3361f10425757250b87906fb03f9430493916914
Masashi Fujita <masashifujita536@gmail.com>
2022-05-05 10:09:22 +0900

    検索条件を表示する機能を追加

--- app/forms/search_questions_form.rb
+++ app/forms/search_questions_form.rb
@@ -10,17 +10,37 @@ class SearchQuestionsForm
   attribute :unit_ids
 
   def search
+    @condition = []
+    university_ids_no_blank = university_ids&.reject(&:blank?)
+    unit_ids_no_blank = unit_ids&.reject(&:blank?)
     relation = Question.with_attached_image.includes({ departments: [:university] }, :questions_units_mediators, { questions_departments_mediators: [:department] })
 
     # 大学名によるquestionの絞り込み
-    relation = relation.by_university_ids(university_ids) if university_ids&.reject(&:blank?).present?
+    if university_ids_no_blank.present?
+      relation = relation.by_university_ids(university_ids)
+      @condition << "大学「#{University.find(university_ids_no_blank).pluck(:name).join('、')}」"
+    end
 
     # 出題年によるquestionの絞り込み
-    relation = relation.by_year(start_year, end_year) if start_year.present? & end_year.present?
+    if start_year.present? & end_year.present?
+      relation = relation.by_year(start_year, end_year)
+      @condition << "出題年「#{start_year} 年 〜 #{end_year} 年」"
+    end
 
     # 単元によるquestionの絞り込み
-    relation = relation.by_unit_ids(unit_ids) if unit_ids&.reject(&:blank?).present?
+    if unit_ids_no_blank.present?
+      relation = relation.by_unit_ids(unit_ids)
+      @condition << "単元「#{Unit.find(unit_ids_no_blank).pluck(:name).join('、')}」"
+    end
 
     relation
+  end
+
+  def display_conditions
+    if @condition.present?
+      "#{@condition.join('／')}で検索した結果を出題年が新しい順に表示しています。"
+    else
+      "全ての問題を出題年が新しい順に表示しています。"
+    end
   end
 end
