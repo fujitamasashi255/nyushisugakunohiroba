36b2c137efa0cb5629e2b004d9345b9f666cb940
Masashi Fujita <masashifujita536@gmail.com>
2022-05-09 11:28:18 +0900

    モデルSearchQuestionsFormの名前をQuestionsSearchFormへ変更

--- app/forms/questions_search_form.rb
+++ app/forms/questions_search_form.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SearchQuestionsForm
+class QuestionsSearchForm
   SORT_TYPES_ENUM = { year_new: 1, created_at_new: 10 }.each_value(&:freeze).freeze
 
   include ActiveModel::Model
@@ -15,7 +15,7 @@ class SearchQuestionsForm
   def initialize(params = {})
     # 条件表示の変数
     @search_conditions = []
-    @sort_condition = I18n.t("activemodel.attributes.search_questions_form.sort_type.year_new")
+    @sort_condition = I18n.t("activemodel.attributes.questions_search_form.sort_type.year_new")
     super(params)
   end
 
@@ -27,40 +27,40 @@ class SearchQuestionsForm
 
     # 大学名によるquestionの絞り込み
     if university_ids_no_blank.present?
-      relation = relation.by_university_ids(university_ids)
+      relation = relation.by_university_ids(university_ids).distinct
       @search_conditions << "#{University.model_name.human}「#{University.find(university_ids_no_blank).pluck(:name).join('、')}」"
     end
 
     # 出題年によるquestionの絞り込み
     if start_year.present? & end_year.present?
-      relation = relation.by_year(start_year, end_year)
+      relation = relation.by_year(start_year, end_year).distinct
       @search_conditions << "#{Question.human_attribute_name(:year)}「#{start_year} 年 〜 #{end_year} 年」"
     end
 
     # 単元によるquestionの絞り込み
     if unit_ids_no_blank.present?
-      relation = relation.by_unit_ids(unit_ids)
+      relation = relation.by_unit_ids(unit_ids).distinct
       @search_conditions << "#{Unit.model_name.human}「#{Unit.find(unit_ids_no_blank).pluck(:name).join('、')}」"
     end
 
     # 並び替え
     case sort_type
     when SORT_TYPES_ENUM[:year_new]
-      relation = relation.order(year: :desc)
-      @sort_condition = I18n.t("activemodel.attributes.search_questions_form.sort_type.year_new")
+      relation = relation.reorder(year: :desc)
+      @sort_condition = I18n.t("activemodel.attributes.questions_search_form.sort_type.year_new")
     when SORT_TYPES_ENUM[:created_at_new]
-      relation = relation.order(created_at: :desc)
-      @sort_condition = I18n.t("activemodel.attributes.search_questions_form.sort_type.created_at_new")
+      relation = relation.reorder(created_at: :desc)
+      @sort_condition = I18n.t("activemodel.attributes.questions_search_form.sort_type.created_at_new")
     end
 
     relation
   end
 
-  def display_conditions
+  def display_search_conditions
     if @search_conditions.present?
-      I18n.t("activemodel.methods.search_questions_form.display_conditions.with_search_conditions", search_conditions: @search_conditions.join("／"), sort_condition: @sort_condition)
+      @search_conditions.join("／")
     else
-      I18n.t("activemodel.methods.search_questions_form.display_conditions.without_search_conditions", sort_condition: @sort_condition)
+      "なし"
     end
   end
 end
