8049791d792d0bd4dc19cfda4d5cb2d44cda2d38
Masashi Fujita <masashifujita536@gmail.com>
2022-05-16 01:01:20 +0900

    universities、departmentsシステムスペックで、値が具体的になるよう修正

--- spec/system/departments_spec.rb
+++ spec/system/departments_spec.rb
@@ -10,68 +10,68 @@ RSpec.describe "Departments", type: :system do
 
     context "大学名が入力されていないとき" do
       it "区分が作成されないこと" do
-        fill_in "区分名", with: "department_name"
+        fill_in "区分名", with: "理系"
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成できませんでした"
         expect(page).to have_selector ".invalid-feedback", text: "大学名 を入力して下さい"
-        expect(page).to have_field "区分名", with: "department_name"
+        expect(page).to have_field "区分名", with: "理系"
       end
     end
 
     context "大学名が入力されているとき" do
       before do
-        fill_in "大学名", with: "university_name"
+        fill_in "大学名", with: "東京"
       end
 
       it "名前のない区分は作成できないこと" do
         fill_in "区分名", with: ""
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成しました"
-        expect(page).to have_selector ".university-name", text: "university_name"
+        expect(page).to have_selector ".university-name", text: "東京"
         expect(page).not_to have_selector ".department-name"
       end
 
       it "1つの区分が作成できること" do
-        fill_in "区分名", with: "department_name"
+        fill_in "区分名", with: "理系"
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成しました"
-        expect(page).to have_selector ".university-name", text: "university_name"
-        expect(page).to have_selector ".department-name", text: "department_name"
+        expect(page).to have_selector ".university-name", text: "東京"
+        expect(page).to have_selector ".department-name", text: "理系"
       end
 
       it "異なる名前の区分を2つ作成できること" do
-        fill_in "区分名", with: "department_name"
+        fill_in "区分名", with: "理系"
         click_on "追加"
         within all(".nested-fields").last do
-          fill_in "区分名", with: "another_department_name"
+          fill_in "区分名", with: "文系"
         end
         expect(all(".nested-fields").count).to eq 2
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成しました"
-        expect(page).to have_selector ".university-name", text: "university_name"
+        expect(page).to have_selector ".university-name", text: "東京"
         expect(all(".department-name").count).to eq 2
-        expect(page).to have_selector ".department-name", text: "department_name"
-        expect(page).to have_selector ".department-name", text: "another_department_name"
+        expect(page).to have_selector ".department-name", text: "理系"
+        expect(page).to have_selector ".department-name", text: "文系"
       end
 
       it "同名の区分は作成できないこと" do
-        fill_in "区分名", with: "department_name"
+        fill_in "区分名", with: "理系"
         click_on "追加"
         within all(".nested-fields").last do
-          fill_in "区分名", with: "department_name"
+          fill_in "区分名", with: "理系"
         end
         expect(all(".nested-fields").count).to eq 2
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成できませんでした"
         expect(page).to have_content "同じ名前の区分を登録することはできません"
-        expect(page).to have_field "大学名", with: "university_name"
-        expect(page).to have_field "区分名", with: "department_name"
+        expect(page).to have_field "大学名", with: "東京"
+        expect(page).to have_field "区分名", with: "理系"
       end
     end
   end
 
   describe "区分編集機能" do
-    let(:university_has_one_department) { create(:university, :has_one_department, name: "university_name", department_name: "department_name") }
+    let(:university_has_one_department) { create(:university, :has_one_department, name: "東京", department_name: "理系") }
 
     before do
       visit edit_admin_university_path(university_has_one_department)
@@ -80,15 +80,15 @@ RSpec.describe "Departments", type: :system do
     it "新しい区分を追加できること" do
       click_on "追加"
       within all(".nested-fields").last do
-        fill_in "区分名", with: "another_department_name"
+        fill_in "区分名", with: "文系"
       end
       expect(all(".nested-fields").count).to eq 2
       click_button "大学・区分を変更する"
       expect(page).to have_content "大学・区分を変更しました"
-      expect(page).to have_selector ".university-name", text: "university_name"
+      expect(page).to have_selector ".university-name", text: "東京"
       expect(all(".department-name").count).to eq 2
-      expect(page).to have_selector ".department-name", text: "department_name"
-      expect(page).to have_selector ".department-name", text: "another_department_name"
+      expect(page).to have_selector ".department-name", text: "理系"
+      expect(page).to have_selector ".department-name", text: "文系"
     end
 
     it "区分を削除できること" do
@@ -96,37 +96,21 @@ RSpec.describe "Departments", type: :system do
       expect(all(".nested-fields").count).to eq 0
       click_button "大学・区分を変更する"
       expect(page).to have_content "大学・区分を変更しました"
-      expect(page).to have_selector ".university-name", text: "university_name"
+      expect(page).to have_selector ".university-name", text: "東京"
       expect(page).not_to have_selector ".department-name"
     end
 
     it "同じ名前の区分を追加できないこと" do
       click_on "追加"
       within all(".nested-fields").last do
-        fill_in "区分名", with: "department_name"
+        fill_in "区分名", with: "理系"
       end
       expect(all(".nested-fields").count).to eq 2
       click_button "大学・区分を変更する"
       expect(page).to have_content "大学・区分を変更できませんでした"
       expect(page).to have_content "同じ名前の区分を登録することはできません"
-      expect(page).to have_field "大学名", with: "university_name"
-      expect(page).to have_field "区分名", with: "department_name"
-    end
-  end
-
-  describe "区分一覧機能" do
-    let(:university_has_departments) { create(:university, :has_departments, name: "university_name", department_counts: 5) }
-
-    before do
-      university_has_departments
-      visit admin_universities_path
-    end
-
-    it "区分一覧が表示されること" do
-      expect(all(".department-name").count).to eq 5
-      university_has_departments.departments.each do |department|
-        expect(page).to have_selector ".department-name", text: department.name
-      end
+      expect(page).to have_field "大学名", with: "東京"
+      expect(page).to have_field "区分名", with: "理系"
     end
   end
 end
--- spec/system/universities_spec.rb
+++ spec/system/universities_spec.rb
@@ -4,8 +4,6 @@ require "rails_helper"
 
 RSpec.describe "Universities", type: :system do
   describe "大学新規作成機能" do
-    let(:university) { create(:university, name: "university_name") }
-
     before do
       visit new_admin_university_path
     end
@@ -19,38 +17,41 @@ RSpec.describe "Universities", type: :system do
 
     context "同名の大学がないとき" do
       it "名前のある大学は作成できること" do
-        fill_in "大学名", with: "university_name"
+        fill_in "大学名", with: "東京"
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成しました"
-        expect(page).to have_content "university_name"
+        expect(page).to have_content "東京"
       end
     end
 
     context "同名の大学があるとき" do
+      before do
+        create(:university, name: "東京")
+      end
+
       it "同じ名前の大学は作成できないこと" do
-        fill_in "大学名", with: university.name
+        fill_in "大学名", with: "東京"
         click_button "大学・区分を作成する"
         expect(page).to have_content "大学・区分を作成できませんでした"
         expect(page).to have_selector ".invalid-feedback", text: "大学名 は既に存在します"
-        expect(page).to have_field "大学名", with: university.name
+        expect(page).to have_field "大学名", with: "東京"
       end
     end
   end
 
   describe "大学編集機能" do
-    let(:university_edit) { create(:university, name: "university_edit_name") }
-    let(:university) { create(:university, name: "university_name") }
+    let(:edit_university) { create(:university, name: "京都") }
 
     before do
-      visit edit_admin_university_path(university_edit)
+      visit edit_admin_university_path(edit_university)
     end
 
     it "大学名を変更できること" do
-      fill_in "大学名", with: "university_edit_another_name"
+      fill_in "大学名", with: "大阪"
       click_button "大学・区分を変更する"
-      expect(current_path).to eq admin_university_path(university_edit)
+      expect(current_path).to eq admin_university_path(edit_university)
       expect(page).to have_content "大学・区分を変更しました"
-      expect(page).to have_content "university_edit_another_name"
+      expect(page).to have_content "大阪"
     end
 
     it "大学名を空欄にはできないこと" do
@@ -61,46 +62,49 @@ RSpec.describe "Universities", type: :system do
     end
 
     it "既に存在する大学と同じ名前には変更できないこと" do
-      fill_in "大学名", with: university.name, fill_options: { clear: :backspace }
+      create(:university, name: "東京")
+      fill_in "大学名", with: "東京", fill_options: { clear: :backspace }
       click_button "大学・区分を変更する"
       expect(page).to have_content "大学・区分を変更できませんでした"
       expect(page).to have_content "大学名 は既に存在します"
-      expect(page).to have_field "大学名", with: university.name
+      expect(page).to have_field "大学名", with: "東京"
     end
   end
 
   describe "大学一覧機能" do
-    let(:university_list) { create_list(:university, 5) }
+    let!(:university_tokyo) { create(:university, :has_one_department, name: "東京", department_name: "理系") }
+    let!(:university_kyoto) { create(:university, :has_one_department, name: "京都", department_name: "文系") }
 
     before do
-      university_list
       visit admin_universities_path
     end
 
-    it "大学が一覧で表示されること" do
-      expect(all(".university-name").count).to eq 5
-      university_list.each do |university|
-        expect(page).to have_selector ".university-name", text: university.name
-      end
+    it "大学・区分が一覧で表示されること" do
+      expect(all(".university-name").count).to eq 2
+      expect(page).to have_selector ".university-name", text: "東京"
+      expect(page).to have_selector ".university-name", text: "京都"
+      expect(all(".department-name").count).to eq 2
+      expect(page).to have_selector ".department-name", text: "理系"
+      expect(page).to have_selector ".department-name", text: "文系"
     end
 
     it "大学名で検索できること" do
-      fill_in "大学名", with: university_list[0].name
+      fill_in "大学名", with: "東京"
       click_button "検索"
-      expect(page).to have_selector ".university-name", text: university_list[0].name
+      expect(page).to have_selector ".university-name", text: "東京"
+      expect(page).not_to have_selector ".university-name", text: "京都"
     end
 
     it "大学を削除できること" do
+      # university_tokyoを削除
       page.accept_confirm("本当に削除しますか") do
-        within first("tr") do
+        within(".university#{university_tokyo.id}-info") do
           click_on "削除"
         end
       end
       expect(page).to have_content "大学・区分を削除しました"
-      expect(all(".university-name").count).to eq 4
-      University.all.each do |university|
-        expect(page).to have_selector ".university-name", text: university.name
-      end
+      expect(all(".university-name").count).to eq 1
+      expect(page).to have_selector ".university-name", text: "京都"
     end
   end
 end
