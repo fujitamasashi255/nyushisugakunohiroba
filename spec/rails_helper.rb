1b9b437bd2cabed2d2eaa6c3700872f0e0e22f28
Masashi Fujita <masashifujita536@gmail.com>
2022-05-19 22:44:49 +0900

    rspec実行時後フリーズしないよう、debugの設定を追加し、springを利用しないよう修正

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
+# rspecの子プロセスに対してdebbugerをactivateしない
+ENV["RUBY_DEBUG_FORK_MODE"] ||= "parent"
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
