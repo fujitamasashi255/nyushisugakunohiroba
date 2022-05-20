b18f8c570df15a98c8252ce25b5474f9f6f95ca7
Masashi Fujita <masashifujita536@gmail.com>
2022-05-20 15:05:31 +0900

    実験

--- spec/support/create_question.rb
+++ spec/support/create_question.rb
@@ -18,8 +18,7 @@ module CreateQuestion
       # 問題文texをコンパイル
       fill_in "texコード", with: tex_code
       click_button "コンパイルする"
-      page.find("#compile-message", text: "コンパイルに成功しました。")
-      # expect(page).to have_content "コンパイルに成功しました。"
+      expect(page).to have_selector("#compile-message"), text: "コンパイルに成功しました。"
     end
     click_button "問題を作成する"
   end
