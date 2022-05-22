1607ce4ceb24224bf082a6f09ccc74f6c5970e7d
Masashi Fujita <masashifujita536@gmail.com>
2022-05-23 08:56:18 +0900

    並び替え時にもサイドバーアイテムがアクティブになるよう修正

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
 module Admin::ApplicationHelper
-  # views/admin/_sidebarでアクティブなメニューを判定
-  def active_if(controller, action)
-    current_page?(controller:, action:) ? "active" : ""
-  end
-
-  def active_if_by_path(*paths)
+  # アクティブなメニューを判定
+  def active_if(*paths)
     active_link?(*paths) ? "active" : ""
   end
 
