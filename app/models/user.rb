1a53069de0b00d6276fb2b1598e705cf460766de
Masashi Fujita <masashifujita536@gmail.com>
2022-05-30 17:36:15 +0900

    rails g sorcery:installを実行後、usersテーブルに必要なカラムを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,22 @@
 #
 # Table name: users
 #
-#  id         :uuid             not null, primary key
-#  name       :string           not null
-#  role       :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id               :uuid             not null, primary key
+#  crypted_password :string
+#  email            :string           default(""), not null
+#  name             :string           not null
+#  role             :integer          not null
+#  salt             :string
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email  (email) UNIQUE
 #
 class User < ApplicationRecord
+  authenticates_with_sorcery!
+
   validates :name, presence: true, length: { maximum: 20 }
   validates :role, presence: true
 
