ac42c6dc2d43a118dcb5e18e015ac1e74f9aabf5
Masashi Fujita <masashifujita536@gmail.com>
2022-06-06 16:33:45 +0900

    パスワードリセットメールのsubject、本文を修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,6 @@ class UserMailer < ApplicationMailer
   def reset_password_email(user)
     @user = User.find(user.id)
     @url  = edit_password_reset_url(@user.reset_password_token)
-    mail(to: user.email, subject: "パスワード変更メール")
+    mail(to: user.email, subject: "【入試数学の広場】パスワード再設定URL の送付")
   end
 end
