cff5c825abdbe2c97429991162e33b65049ebb49
Masashi Fujita <masashifujita536@gmail.com>
2022-06-15 18:55:07 +0900

    avatarを、ユーザー編集、詳細、サイドバーで表示する際、variantを使用するよう修正

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 module UserDecorator
+  def avatar_url(version)
+    case version
+    when :profile
+      avatar.variant(resize_to_fill: [80, 80], saver: { strip: true, quality: 60 })
+    when :sidebar
+      avatar.variant(resize_to_fill: [32, 32], saver: { strip: true })
+    else
+      avatar
+    end
+  end
 end
