1caac3439d9913c49e1aadefb6eac47791cc5a48
Masashi Fujita <masashifujita536@gmail.com>
2022-06-15 19:21:18 +0900

    current_userに対してデコレータを利用できるよう修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
 
   include Pagy::Backend
 
+  def current_user
+    (u = super) && ActiveDecorator::Decorator.instance.decorate(u)
+  end
+
   def redirect_if_logged_in
     redirect_to root_path, danger: t("messages.still_logged_in") if logged_in?
   end
