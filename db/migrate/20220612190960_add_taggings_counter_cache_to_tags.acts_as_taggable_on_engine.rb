930d86ad79c7afdd698c39b8b4984a0c4a509e86
Masashi Fujita <masashifujita536@gmail.com>
2022-06-17 09:27:37 +0900

    マイグレーション ファイルに say_with_timeで説明を追加

--- db/migrate/20220612190960_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
+++ db/migrate/20220612190960_add_taggings_counter_cache_to_tags.acts_as_taggable_on_engine.rb
@@ -4,15 +4,19 @@
 
 class AddTaggingsCounterCacheToTags < ActiveRecord::Migration[6.0]
   def self.up
-    add_column ActsAsTaggableOn.tags_table, :taggings_count, :integer, default: 0
+    say_with_time("ActsAsTaggableOn.tags_tableにtaggings_countカラムを追加") do
+      add_column ActsAsTaggableOn.tags_table, :taggings_count, :integer, default: 0
 
-    ActsAsTaggableOn::Tag.reset_column_information
-    ActsAsTaggableOn::Tag.find_each do |tag|
-      ActsAsTaggableOn::Tag.reset_counters(tag.id, ActsAsTaggableOn.taggings_table)
+      ActsAsTaggableOn::Tag.reset_column_information
+      ActsAsTaggableOn::Tag.find_each do |tag|
+        ActsAsTaggableOn::Tag.reset_counters(tag.id, ActsAsTaggableOn.taggings_table)
+      end
     end
   end
 
   def self.down
-    remove_column ActsAsTaggableOn.tags_table, :taggings_count
+    say_with_time("ActsAsTaggableOn.tags_tableからtaggings_countカラムを削除") do
+      remove_column ActsAsTaggableOn.tags_table, :taggings_count
+    end
   end
 end
