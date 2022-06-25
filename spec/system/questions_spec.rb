f2a6561b8044808eb0add1a6ae0c04b21d7bf8ef
Masashi Fujita <masashifujita536@gmail.com>
2022-06-25 16:41:40 +0900

    問題詳細画面のQuestionシステムスペックを作成

--- spec/system/questions_spec.rb
+++ spec/system/questions_spec.rb
@@ -9,15 +9,15 @@ RSpec.describe "Questions", type: :system, js: true do
   let!(:user) { create(:user, name: "TEST", email: "test@example.com", password: "1234abcd", password_confirmation: "1234abcd", role: :admin) }
 
   before do
-    user1 = create(:user, name: "TEST1", email: "test1@example.com", password: "1234abcd", password_confirmation: "1234abcd", role: :admin)
-    user2 = create(:user, name: "TEST2", email: "test2@example.com", password: "1234abcd", password_confirmation: "1234abcd", role: :admin)
+    user1 = create(:user, name: "USER1", email: "test1@example.com", password: "1234abcd", password_confirmation: "1234abcd", role: :admin)
+    user2 = create(:user, name: "USER2", email: "test2@example.com", password: "1234abcd", password_confirmation: "1234abcd", role: :admin)
     # 京都大学、名古屋大学、東京大学の問題
     @question_kyoto = create(:question, :full_custom, year: 2020, department: department_of_kyoto, question_number: 5, unit: "図形と計量")
     @question_nagoya = create(:question, :full_custom, year: 2010, department: department_of_nagoya, question_number: 7, unit: "三角関数")
     @question_tokyo = create(:question, :full_custom, year: 2000, department: department_of_tokyo, question_number: 10, unit: "数と式・集合と論理")
     # 京都大学の解答：2個、名古屋大学の解答：1個、東京大学の解答：0個
-    create(:answer, question: @question_kyoto, user: user1, tag_names: "tag1")
-    create(:answer, question: @question_kyoto, user: user2, tag_names: "tag2")
+    create(:answer, question: @question_kyoto, user: user1, tag_names: "tag1", point: "ポイント1")
+    create(:answer, question: @question_kyoto, user: user2, tag_names: nil, point: nil)
     create(:answer, question: @question_nagoya, user: user1, tag_names: "tag1")
 
     # ログイン
@@ -203,6 +203,69 @@ RSpec.describe "Questions", type: :system, js: true do
         expect(all(".question-card")[0]).to have_content "京都"
         expect(all(".question-card")[1]).to have_content "名古屋"
         expect(page).not_to have_content "東京"
+      end
+    end
+  end
+
+  fdescribe "問題詳細機能" do
+    context "userが問題の解答を作成しており、他にも問題の解答を作成しているユーザーがいるとき" do
+      before do
+        @answer = create(:answer, question: @question_kyoto, user:, tag_names: "tag1")
+        visit question_path(@question_kyoto)
+      end
+
+      it "問題の情報が表示されること" do
+        expect(page).to have_selector ".question-show .year", text: "2020"
+        expect(page).to have_selector ".question-show .university", text: "京都"
+        expect(page).to have_selector ".question-show .department-name", text: "文系5"
+        expect(page).to have_selector ".question-show .unit-name", text: "図形と計量"
+        expect(page).to have_selector ".question-show .question-image img"
+      end
+
+      it "他のユーザーの解答が表示されること" do
+        expect(page).to have_content "他のユーザーの解答"
+        expect(page).to have_selector ".answer-card .user-name", text: "USER1"
+        expect(page).to have_selector ".answer-card .tags", text: "tag1"
+        expect(page).to have_selector ".answer-card .point", text: "ポイント1"
+        expect(page).to have_selector ".answer-card .user-name", text: "USER2"
+        expect(page).to have_selector ".answer-card .tags", text: "なし"
+        expect(page).to have_selector ".answer-card .point-container", text: "なし"
+      end
+
+      it "自分の解答は表示されないこと" do
+        expect(page).not_to have_selector ".answer-card .user-name", text: "TEST"
+      end
+
+      it "解答編集ページへのリンクが表示されること" do
+        expect(page).to have_link "解答を編集する", href: edit_answer_path(@answer)
+      end
+
+      it "解答新規作成ページへのリンクは表示されないこと" do
+        expect(page).not_to have_link "解答を作成する", href: new_question_answer_path(@question_kyoto)
+      end
+    end
+
+    context "ユーザーが問題の解答を作成しておらず、問題の解答を作成している他のユーザーがいるとき" do
+      before do
+        visit question_path(@question_kyoto)
+      end
+
+      it "解答編集ページへのリンクが表示されないこと" do
+        expect(page).not_to have_content "解答を編集する"
+      end
+
+      it "解答新規作成ページへのリンクが表示されること" do
+        expect(page).to have_link "解答を作成する", href: new_question_answer_path(@question_kyoto)
+      end
+    end
+
+    context "問題の解答を作成しているユーザーが1人もいないとき" do
+      before do
+        visit question_path(@question_tokyo)
+      end
+
+      it "他のユーザーの解答が表示されないこと" do
+        expect(page).not_to have_content "他のユーザーの解答"
       end
     end
   end
