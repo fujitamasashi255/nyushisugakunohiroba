c304ef9870466d14401313bd99541a82380b585b
Masashi Fujita <masashifujita536@gmail.com>
2022-06-29 16:25:37 +0900

    解答詳細、自分の作成した解答一覧ページにtwitter共有カードの設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -52,13 +52,14 @@ module ApplicationHelper
       title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
       description: :description,   # 上に同じ
       url: request.url,
+      type: "website",
       image: "https://example.com/hoge.png"
     }
   end
 
   def default_twitter_card
     {
-      card: "summary_large_image",
+      card: "summary", # または summary_large_image
       site: "@hogehoge"            # twitter ID
     }
   end
