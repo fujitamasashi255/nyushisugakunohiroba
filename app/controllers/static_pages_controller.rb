1b9eaa81b9e19c17d9f5d3dd5ca7a95a0a9a2de2
Masashi Fujita <masashifujita536@gmail.com>
2022-07-04 17:23:41 +0900

    トップページに問題検索フォーム、人気の問題、最新の解答を表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,5 +3,10 @@
 class StaticPagesController < ApplicationController
   skip_before_action :require_login, only: [:top]
 
-  def top; end
+  def top
+    @questions_search_form = QuestionsSearchForm.new(specific_search_condition: QuestionsSearchForm::SPECIFIC_CONDITIONS_ENUM[:all_data], sort_type: "answers_many")
+    @questions = @questions_search_form.search.limit(2).with_attached_image.includes({ departments: :university }, :questions_units_mediators, { questions_departments_mediators: :department })
+    @question_id_to_answer_id_hash_of_user = logged_in? ? current_user.question_id_to_answer_id_hash : {}
+    @answers = Answer.all.includes(:question, :rich_text_point, :tags, user: { avatar_attachment: :blob }).order(created_at: :desc).limit(2)
+  end
 end
