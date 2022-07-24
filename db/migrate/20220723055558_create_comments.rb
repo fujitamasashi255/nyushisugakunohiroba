50b67ff47920f6a3ff21789d2209fc84213df634
Masashi Fujita <masashifujita536@gmail.com>
2022-07-24 13:20:59 +0900

    commentのbodyにはActionTextを用いるので、テーブルからbodyカラムを削除

--- db/migrate/20220723055558_create_comments.rb
+++ db/migrate/20220723055558_create_comments.rb
@@ -4,7 +4,6 @@ class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments, id: :uuid do |t|
       t.references :commentable, polymorphic: true, null: false, type: :uuid
-      t.text :body, null: false
       t.references :user, null: false, foreign_key: true, type: :uuid
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -70,7 +70,6 @@ ActiveRecord::Schema.define(version: 2022_07_23_055558) do
   create_table "comments", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "commentable_type", null: false
     t.uuid "commentable_id", null: false
-    t.text "body", null: false
     t.uuid "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
