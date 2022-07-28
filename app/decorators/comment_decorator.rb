33d5ff01efab64b17f31bb545ee8e80563f6541a
Masashi Fujita <masashifujita536@gmail.com>
2022-07-28 13:32:46 +0900

    commentデコレータの、コメント作成後経過時間表示メソッドの名前を修正

--- app/decorators/comment_decorator.rb
+++ app/decorators/comment_decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module CommentDecorator
-  def updated_elapsed_time
+  def created_elapsed_time
     elapsed_seconds = (Time.current - created_at).floor
     [[60, "秒"], [60, "分"], [24, "時間"], [7, "日"], [4.3, "週間"], [12, "カ月"]].each.with_index.inject(elapsed_seconds) do |result, (array, idx)|
       q, r = result.divmod(array[0])
