ea0377da8c9a5feb9841a043676bdc7b982fdd0b
Masashi Fujita <masashifujita536@gmail.com>
2022-08-16 17:15:56 +0900

    iframeにCDNからのレスポンスを埋め込めるようデフォルトのHTTPヘッダーを修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,7 @@ Rails.application.configure do
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
+  config.action_dispatch.default_headers["X-Frame-Options"] = "ALLOW-FROM https://#{ENV['CDN_HOST']}"
   config.public_file_server.headers = {
     "Cache-Control" => "max-age=31536000, must-revalidate"
   }
