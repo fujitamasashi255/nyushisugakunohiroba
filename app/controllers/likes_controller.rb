74ddf718f9fad17c20ce99a04c3c1dc87934f2f5
Masashi Fujita <masashifujita536@gmail.com>
2022-08-18 14:26:58 +0900

    いいねボタンの非同期処理をJS→サーバーに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,12 +3,14 @@
 class LikesController < ApplicationController
   def create
     answer_id = params[:answer_id]
-    Like.create!(user: current_user, answer_id:)
-    @answer = Answer.find(answer_id)
+    like = Like.create!(user: current_user, answer_id:)
+    answer = Answer.find(answer_id)
+    render json: { destroy_path: answer_like_path(answer, like) }
   end
 
   def destroy
     Like.find(params[:id]).destroy!
-    @answer = Answer.find(params[:answer_id])
+    answer = Answer.find(params[:answer_id])
+    render json: { create_path: answer_likes_path(answer) }
   end
 end
