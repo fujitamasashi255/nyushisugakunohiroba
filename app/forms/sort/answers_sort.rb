21ebea7c00d0233bf28716eb916cec5f4dbd1e0a
Masashi Fujita <masashifujita536@gmail.com>
2022-09-20 11:18:51 +0900

    並び替えを担当するSortクラス群を作成

---
+++ app/forms/sort/answers_sort.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Sort
+  class AnswersSort < SortBase
+    YEAR_NEW = lambda { |answers|
+      Answer.joins(:question).where(answers: { id: answers.select(:id) }).select("answers.*, questions.year").order(Arel.sql("questions.year desc"))
+    }
+    UPDATED_AT_NEW = ->(answers) { answers.order(updated_at: :desc) }
+    LIKE_MANY = ->(answers) { answers.order(likes_count: :desc) }
+    COMMENT_NEW = lambda { |answers|
+      sql_to_get_newest_comment_date = "COALESCE(MAX(comments.created_at), '0001-01-01')"
+      Answer.left_joins(:comments).where(answers: { id: answers.select(:id) }).select("answers.*, #{sql_to_get_newest_comment_date}").group("answers.id").order(Arel.sql("#{sql_to_get_newest_comment_date} desc"))
+    }
+  end
+end
